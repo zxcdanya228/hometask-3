8e38b86e0b59ae8b039059e7eb60161eb27f949d
zxcdanya228 <zxcdanya228@gmail.com>
2022-10-15 15:43:46 +0300

    Add 8.py

---
+++ 8.py
@@ -0,0 +1,22 @@
+stack = []
+
+while True:
+    command = input().strip()
+    if command.startswith('push'):
+        number = int(command.split()[1])
+        stack.append(number)
+        print('ok')
+    elif command == 'pop':
+        print(stack.pop())
+    elif command == 'back':
+        print(stack[-1])
+    elif command == 'size':
+        print(stack)
+    elif command == 'clear':
+        stack.clear()
+        print('ok')
+    elif command == 'exit':
+        print('bye')
+        break
+    else:
+        print('error')
